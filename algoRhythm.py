ffd160abe6dca5c0dcc317b65c61b8e170c91755
c-roberts <36252679+c-roberts@users.noreply.github.com>
2019-03-03 16:07:39 -0600

    Started comparison function
    Added delta_time function

--- algoRhythm.py
+++ algoRhythm.py
@@ -11,7 +11,7 @@ def algoRhythm(audio_path, sheet_music, BPM, rhythm_leniency, pitch_leniency):
     # Initialize files and globals
     signal, sr = librosa.load(audio_path, sr=None)
     actual_signal = extract_sheet_music(sheet_music)
-    create_freq_dict()
+    #create_freq_dict()
 
 
     # Extract rhythm from user and from sheet music
@@ -20,16 +20,24 @@ def algoRhythm(audio_path, sheet_music, BPM, rhythm_leniency, pitch_leniency):
 
 
     # Extract pitch from user and from sheet music
-    user_pitch = extract_user_pitch(signal)
-    actual_pitch = extract_actual_pitch(actual_signal)
+    #user_pitch = extract_user_pitch(signal)
+    #actual_pitch = extract_actual_pitch(actual_signal)
 
 
     # Compare user and correct rhythm
     rhythm_score, rhythm_errors = compare_rhythm(user_rhythm, actual_rhythm, rhythm_leniency)
-    pitch_score, pitch_errors = compare_pitch(user_pitch, actual_pitch, pitch_leniency)
+    #pitch_score, pitch_errors = compare_pitch(user_pitch, actual_pitch, pitch_leniency)
 
     return
 
+def testing():
+    mySignal, sr = librosa.load("Testing Data/test5.wav", sr=None)
+    test=extract_user_rhythm(mySignal)
+    print(test)
+    d_test= calculate_delta_time(test)
+    print(d_test)
+
+testing()
 
 
 ###################################
@@ -47,7 +55,7 @@ def extract_user_rhythm(signal):
 
     '''
     rhythm_arr = librosa.onset.onset_detect(signal, sr=sr, units='time')
-    raise NotImplementedError('Function not implemented.')
+    
     return rhythm_arr
 
 
@@ -62,6 +70,17 @@ def extract_actual_rhythm(actual_signal, user_tempo):
     raise NotImplementedError('Function not implemented.')
     return rhythm_act
 
+def calculate_delta_time(onsets):
+    '''
+    Inputs: onsets from user signal or sheet music rhythm
+    Outputs: difference in time from each onset to the next
+    '''
+    d_time=np.zeros(onsets.shape)
+    for i in range(1, onsets.size-1, 1):
+        d_time[i]=onsets[i]-onsets[i-1]
+
+    return d_time
+
 
 def compare_rhythm(user_rhythm, actual_rhythm, leniency):
     '''
@@ -72,9 +91,29 @@ def compare_rhythm(user_rhythm, actual_rhythm, leniency):
     '''
     errors = []
     score=0
-    raise NotImplementedError('Function not implemented.')
 
+    diff = actual_rhythm.size - user_rhythm.size # extra or missing notes
+
+    # calculate delta_time and align first note to zero
+    d_user = calculate_delta_time(user_rhythm)
+    d_actual = calculate_delta_time(actual_rhythm)
+
+    for i in range(1,d_actual.size):
+        if ( (d_actual-leniency ) < d_user < ( d_actual+leniency )):
+            # correct, move on
+            print("noice", i)
+
+        else:
+            # add timestep to errors
+            errors.append(user_rhythm[i])
+
+            # try to figure out if extra
+
+    score = (len(errors) / actual_rhythm.size) * 100
+
+    raise NotImplementedError('Function not implemented.')
     return score, errors
+
 
 def extract_sheet_music(sheet_music):
     '''
@@ -139,17 +178,16 @@ def create_freq_dict(tuning_pitch=440):
     global notes
     freq_chart=[]
     
-
     notes = [	"C0","C#0","D0","D#0","E0","F0","F#0","G0","G#0","A0","A#0","B0",
-		"C1","C#1","D1","D#1","E1","F1","F#1","G1","G#1","A1","A#1","B1",
-		"C2","C#2","D2","D#2","E2","F2","F#2","G2","G#2","A2","A#2","B2",
-		"C3","C#3","D3","D#3","E3","F3","F#3","G3","G#3","A3","A#3","B3",
-		"C4","C#4","D4","D#4","E4","F4","F#4","G4","G#4","A4","A#4","B4",
-		"C5","C#5","D5","D#5","E5","F5","F#5","G5","G#5","A5","A#5","B5",
-		"C6","C#6","D6","D#6","E6","F6","F#6","G6","G#6","A6","A#6","B6",
-		"C7","C#7","D7","D#7","E7","F7","F#7","G7","G#7","A7","A#7","B7",
-		"C8","C#8","D8","D#8","E8","F8","F#8","G8","G#8","A8","A#8","B8",
-		"C9","C#9","D9","D#9","E9","F9","F#9","G9","G#9","A9","A#9","B9" ]
+                "C1","C#1","D1","D#1","E1","F1","F#1","G1","G#1","A1","A#1","B1",
+                "C2","C#2","D2","D#2","E2","F2","F#2","G2","G#2","A2","A#2","B2",
+                "C3","C#3","D3","D#3","E3","F3","F#3","G3","G#3","A3","A#3","B3",
+                "C4","C#4","D4","D#4","E4","F4","F#4","G4","G#4","A4","A#4","B4",
+                "C5","C#5","D5","D#5","E5","F5","F#5","G5","G#5","A5","A#5","B5",
+                "C6","C#6","D6","D#6","E6","F6","F#6","G6","G#6","A6","A#6","B6",
+                "C7","C#7","D7","D#7","E7","F7","F#7","G7","G#7","A7","A#7","B7",
+                "C8","C#8","D8","D#8","E8","F8","F#8","G8","G#8","A8","A#8","B8",
+                "C9","C#9","D9","D#9","E9","F9","F#9","G9","G#9","A9","A#9","B9" ]
     
     A4_INDEX = 57
 
@@ -194,4 +232,5 @@ def find_closest(pitch):
             min_distance = cur_distance
         
     return (index-1)
+
 
